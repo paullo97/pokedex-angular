1a6ca679cc053129cba87fd8fcdde1a238a2683d
paullo97 <paulo.cesar97@hotmail.com>
2022-07-19 19:20:01 -0300

    include reference components and create first concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Result } from './core/services/models/pokemons.model';
+import { PokemonService } from './core/services/pokemons.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pokedex';
+export class AppComponent implements OnInit
+{
+    public listPokemons: Array<Result> = [];
+
+    constructor(private readonly pokemonService: PokemonService)
+    { }
+
+    public ngOnInit(): void
+    {
+        this.pokemonService.getPokemonslist().subscribe((data) => this.listPokemons = data);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ListComponent } from './components/list/list.component';
+import { SearchComponent } from './components/search/search.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    ListComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
+    CoreModule,
     AppRoutingModule
   ],
   providers: [],
